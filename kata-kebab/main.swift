f51cf495ea62a2acd3a10a73a1c2011d8ac0b6f8
charles nseke <nsekecharles@gmail.com>
2016-05-07 23:43:52 +0200

    Initial Commit

---
+++ kata-kebab/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  kata-kebab
+//
+//  Created by Charles NSEKE on 07/05/2016.
+//  Copyright © 2016 Charles NSEKE. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
